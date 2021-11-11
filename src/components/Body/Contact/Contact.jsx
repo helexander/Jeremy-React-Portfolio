6cb36a3ebc7ef3500dab874233cd25a0d97d0b3a
helexander <jeremychee97@gmail.com>
2021-11-11 23:53:18 +1100

    added first layer for background pop

--- src/components/Body/Contact/Contact.jsx
+++ src/components/Body/Contact/Contact.jsx
@@ -7,17 +7,19 @@ const Contact = () => {
     return (
         <div className={styles.contact}>
             <Separator />
-            <label className={styles.contact_title}>Contact</label>
             <div className={styles.contact_container}>
-                <div className={styles.contact_left}>
-                    <p>Do you wish to connect or collaborate? Contact me on any of the following platforms. </p>
-                    <Socials />
-                </div>
-                <div className={styles.download}>
-                    <a download alt="Jeremys Resume" href={require("../../../assets/resume_jeremy.pdf").default}>
-                        <i className={`fi fi-rr-download ${styles.download_icon}`}></i>
-                        My Resume
-                    </a>
+                <label className={styles.contact_title}>Contact</label>
+                <div className={styles.contact_container}>
+                    <div className={styles.contact_left}>
+                        <p>Do you wish to connect or collaborate? Contact me on any of the following platforms. </p>
+                        <Socials />
+                    </div>
+                    <div className={styles.download}>
+                        <a download alt="Jeremys Resume" href={require("../../../assets/resume_jeremy.pdf").default}>
+                            <i className={`fi fi-rr-download ${styles.download_icon}`}></i>
+                            My Resume
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
